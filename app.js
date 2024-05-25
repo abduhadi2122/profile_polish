e8fe67680e17f2d1e87b8826c1354c23a7fcc758
Hadi Abdul <abdulhadi2122@gmail.com>
2024-05-25 17:15:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ document.getElementById('profileForm').addEventListener('submit', async function
     formData.append('email', emailInput);
 
     // Fetch the test endpoint
-    const testResponse = await fetch('https://d5a7-2604-3d09-aa7a-9880-3060-2612-47aa-9033.ngrok-free.app/api/test');
+    const testResponse = await fetch('https://59bc-2604-3d09-aa7a-9880-3060-2612-47aa-9033.ngrok-free.app/api/test');
     const testResult = await testResponse.json();
     console.log(testResult); // Log the actual JSON data from the test endpoint
    
